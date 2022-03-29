cd6c90ef2f3c8ae4a63d32aea131efa569ae739a
daieeen <101250985+daieeen@users.noreply.github.com>
2022-03-30 00:59:13 +0900

    7

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const canvas = document.getElementById("jsCanvas");
 
+let painting = false;
+
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
 }
 
 function onMouseDown(event) {
-    console.log(event);
+    painting = true;
+}
+
+function onmouseup(event) {
+    painting = false;
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown)
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onmouseup);
 }
